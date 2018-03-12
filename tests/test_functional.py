9caddf8023a0d3998f9640f083454c7115bf8352
chrzyki <christoph@functor-argument.net>
2018-03-12 11:08:36 +0100

    Update scaffolding etc.

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -7,6 +7,10 @@ pytest_plugins = ['clld']
     "method,path",
     [
         ('get_html', '/'),
+        ('get_html', '/contributions'),
+        ('get_html', '/contributions/5'),
+        ('get_html', '/units'),
+        ('get_html', '/units/9'),
     ])
 def test_pages(app, method, path):
     getattr(app, method)(path)
